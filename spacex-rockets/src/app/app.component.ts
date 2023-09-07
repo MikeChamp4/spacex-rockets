b406c323846e2838cea207a45969549f718d8704
Miguel Enrique Zambrano Contreras <miguel.zambrano@inetum.com>
2023-09-07 16:04:51 +0200

    carga 1 api

--- spacex-rockets/src/app/app.component.ts
+++ spacex-rockets/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spacex-rockets';
+
+  data: any[] = [];
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit() {
+    this.http.get('https://api.spacexdata.com/v4/rockets')
+    .subscribe((data: any) => {
+      this.data = data;
+    })
+  }
 }
